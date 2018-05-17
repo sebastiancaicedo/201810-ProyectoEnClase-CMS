0d0935e99d510be7fae98b64e9c506fce35ea3c7
sebastiancaicedo <sebastiancaicedo@uninorte.edu.co>
2018-05-17 15:21:04 -0500

    agregada ruta a pagina de curso

--- src/components/private.js
+++ src/components/private.js
@@ -4,6 +4,7 @@ import Home from './home.js';
 import { Route } from 'react-router-dom';
 
 import Landing from './landing.js';
+import Course from './course.js';
 
 class Private extends React.Component{
 
@@ -17,6 +18,7 @@ class Private extends React.Component{
             <div className='private'>
                 <Route exact path='/' component={(props) => <Landing {...props} /> } />
                 <Route path='/home' component={(props) => <Home {...props} {...this.props} />} />
+                <Route path='/course/:courseId' component={(props)=> <Course {...props} /> } />
             </div>
         );
     }
